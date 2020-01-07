7f362d66950d80faeebe8727fc251366b9f9bf8f
muchomorek2012 <rpozdziak3@gmail.com>
2020-01-07 19:12:13 +0100

    Index struct

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app/styles/main.css">
+    <title>To je moje portfolio!</title>
+</head>
+<body>
+
+    <header>
+        <a class="logo"><h1>Rafał</h1></a>
+        <p>To jest moje motto życiowe!</p>
+
+        <ul class="nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About me</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+    
+    <main>
+
+        <section class="about">
+        
+            <img src="./." alt="To ja!">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam culpa quam sapiente aut et ex, exercitationem veniam voluptates! Eum quo quas provident modi labore, voluptatum quae odio eligendi suscipit laborum!</p>
+
+            <section class="gallery">
+                <a href="#" target="_blank"><img src="./." alt="To ja!"></a>
+                <a href="#" target="_blank"><img src="./." alt="To ja!"></a>
+                <a href="#" target="_blank"><img src="./." alt="To ja!"></a>
+                <a href="#" target="_blank"><img src="./." alt="To ja!"></a>
+                <a href="#" target="_blank"><img src="./." alt="To ja!"></a>
+                <a href="#" target="_blank"><img src="./." alt="To ja!"></a>
+            </section>
+
+        </section>
+
+        <section class="blog">
+
+            <article>
+                <img src="//" alt="Podpis">
+                <h1>Tytuł</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus distinctio iste voluptas laboriosam vero consequuntur ullam explicabo, itaque quaerat quos odio dolorem saepe animi. Sint quia iusto voluptatem maiores quidem!</p>
+                <h3>07.01.2020r. 19:11:70</h3>
+                <a href="#">Czytaj dalej ...</a>
+
+            </article>
+        
+            <article>
+                <img src="//" alt="Podpis">
+                <h1>Tytuł</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus distinctio iste voluptas laboriosam vero consequuntur ullam explicabo, itaque quaerat quos odio dolorem saepe animi. Sint quia iusto voluptatem maiores quidem!</p>
+                <h3>07.01.2020r. 19:11:70</h3>
+                <a href="#">Czytaj dalej ...</a>
+
+            </article>
+
+            <article>
+                <img src="//" alt="Podpis">
+                <h1>Tytuł</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus distinctio iste voluptas laboriosam vero consequuntur ullam explicabo, itaque quaerat quos odio dolorem saepe animi. Sint quia iusto voluptatem maiores quidem!</p>
+                <h3>07.01.2020r. 19:11:70</h3>
+                <a href="#">Czytaj dalej ...</a>
+
+            </article>
+
+            <!-- PAGINACJA ZRÓB PÓŹNIEJ BO TERA PADAM NA RYJ -->
+
+        </section>
+
+    </main>
+
+    <footer>
+
+        <p>&copy; 2020 | All rights reserved!</p>
+
+    </footer>
+    
+</body>
+</html>
